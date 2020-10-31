c4fb1859e0e2a5ed1d173c81c65473a824b57337
arasopraza <aras.opraza@gmail.com>
2020-10-31 12:44:13 +0700

    Initial Commit :tada:

---
+++ src/Kelipatan.java
@@ -0,0 +1,18 @@
+/**
+ *
+ * @author
+ * NAMA     : Arsy Opraza Akma
+ * KELAS    : IF1
+ * NIM      : 10119001
+ * Deskripsi Program: Program ini menampilkan
+ * kelipatan 3.5
+ */
+
+public class Kelipatan {
+    public static void main(String[] args) {
+
+        for (double i = 3.5; i <= 35; i+=3.5) {
+            System.out.println(i);
+        }
+    }
+}
